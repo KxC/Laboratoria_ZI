b5969706ca893c4da937803cdaddb158bee9aeca
Krzysztof Cieślewicz <plasticman@poczta.fm>
2012-11-07 19:56:58 +0100

    Commit programu SVD

--- Lab 3/Form1.Designer.cs
+++ Lab 3/Form1.Designer.cs
@@ -29,43 +29,34 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 38);
+            this.button1.Location = new System.Drawing.Point(118, 8);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(863, 334);
+            this.button1.Size = new System.Drawing.Size(100, 27);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Zrób";
+            this.button1.Text = "Oblicz";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(147, 20);
-            this.textBox1.TabIndex = 1;
-            // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 378);
+            this.textBox2.Location = new System.Drawing.Point(12, 41);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(863, 156);
+            this.textBox2.Size = new System.Drawing.Size(296, 493);
             this.textBox2.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(887, 546);
+            this.ClientSize = new System.Drawing.Size(341, 546);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -77,7 +68,6 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
     }
 }
--- Lab 3/Form1.cs
+++ Lab 3/Form1.cs
@@ -41,6 +41,23 @@ namespace Lab_3
                     wynik[j, i] = wynik[j, i] - srednie[i];
                 }
             }
+
+            double[,] iloczyn_wektorow = new double[9, 9];
+            double iloczyn = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = i + 1; j < 9; j++)
+                {
+                    for (int k = 0; k < 12; k++)
+                    {
+                        iloczyn = wynik[k, i] * wynik[k, j];
+                        iloczyn_wektorow[i, j] += iloczyn;
+                    }
+                    
+                }
+
+            }
+
             double[] dlugosci_wektorow = new double[9];
             for (int i = 0; i < 9; i++)
             {
@@ -50,33 +67,152 @@ namespace Lab_3
                 }
                 dlugosci_wektorow[i] = Math.Sqrt(dlugosci_wektorow[i]);
             }
-            double[] iloczyny = new double[9];
-
-            for (int i = 0; i < 9; i++)
-            {
-                for (int j = 0; j < 9; j++)
-                {
-                    iloczyny[i] = wynik[j, i];
-                }
-            }
 
             double[,] przed_svd = new double[9, 9];
             for (int i = 0; i < 9; i++)
             {
                 for (int j = i + 1; j < 9; j++)
                 {
-                    przed_svd[i, j] = (dlugosci_wektorow[i] * dlugosci_wektorow[j]) / odleglosci[i, j];
+                    przed_svd[i, j] = iloczyn_wektorow[i, j] / (dlugosci_wektorow[i] * dlugosci_wektorow[j]);
                 }
             }
-            double[,] po_svd = new double[8, 8];
-            double[] dl_po_svd = new double[9];
+            string[] do_wyswietlenia = new string[9] { "C2", "C3", "C4", "C5", "M1", "M2", "M3", "M4", "0" };
+            textBox2.Text = "Przed zastosowaniem SVD:\r\n";
+            textBox2.Text += "      C1  ||  C2  ||  C3  ||  C4  ||  C5  ||  M1  ||  M2  ||  M3\r\n";
+
+            textBox2.Text += do_wyswietlenia[0] + "  ";
+            textBox2.Text += Math.Round(przed_svd[0, 1], 2);    
+            textBox2.Text += "\r\n";
+
+            textBox2.Text += do_wyswietlenia[1] + "  ";
+            textBox2.Text += Math.Round(przed_svd[0, 2], 2) + "  " + Math.Round(przed_svd[1, 2], 2);
+            textBox2.Text += "\r\n";
+
+            textBox2.Text += do_wyswietlenia[2] + "  ";
+            textBox2.Text += Math.Round(przed_svd[0, 3], 2) + "  " + Math.Round(przed_svd[1, 3], 2) + "  " + Math.Round(przed_svd[2, 3], 2);
+            textBox2.Text += "\r\n";
+
+            textBox2.Text += do_wyswietlenia[3] + "  ";
+            textBox2.Text += Math.Round(przed_svd[0, 4], 2) + "  " + Math.Round(przed_svd[1, 4], 2) + " " + Math.Round(przed_svd[2, 4], 2) + " " + Math.Round(przed_svd[3, 4], 2);
+            textBox2.Text += "\r\n";
+
+            textBox2.Text += do_wyswietlenia[4] + "  ";
+            textBox2.Text += Math.Round(przed_svd[0, 5], 2) + " " + Math.Round(przed_svd[1, 5], 2) + " " + Math.Round(przed_svd[2, 5], 2) + " " + Math.Round(przed_svd[3, 5], 2) + " " + Math.Round(przed_svd[4, 5], 2);
+            textBox2.Text += "\r\n";
+
+            textBox2.Text += do_wyswietlenia[5] + "  ";
+            textBox2.Text += Math.Round(przed_svd[0, 6], 2) + " " + Math.Round(przed_svd[1, 6], 2) + " " + Math.Round(przed_svd[2, 6], 2) + " " + Math.Round(przed_svd[3, 6], 2) + " " + Math.Round(przed_svd[4, 6], 2) + " " + Math.Round(przed_svd[5, 6], 2);
+            textBox2.Text += "\r\n";
+
+            textBox2.Text += do_wyswietlenia[6] + "  ";
+            textBox2.Text += Math.Round(przed_svd[0, 7], 2) + " " + Math.Round(przed_svd[1, 7], 2) + " " + Math.Round(przed_svd[2, 7], 2) + " " + Math.Round(przed_svd[3, 7], 2) + " " + Math.Round(przed_svd[4, 7], 2) + " " + Math.Round(przed_svd[5, 7], 2) + " " + Math.Round(przed_svd[6, 7], 2);
+            textBox2.Text += "\r\n";
+
+            textBox2.Text += do_wyswietlenia[7] + "  ";
+            textBox2.Text += Math.Round(przed_svd[0, 8], 2) + " " + Math.Round(przed_svd[1, 8], 2) + " " + Math.Round(przed_svd[2, 8], 2) + " " + Math.Round(przed_svd[3, 8], 2) + " " + Math.Round(przed_svd[4, 8], 2) + " " + Math.Round(przed_svd[5, 8], 2) + " " + Math.Round(przed_svd[6, 8], 2) + " " + Math.Round(przed_svd[7, 8], 2);
+            textBox2.Text += "\r\n";
+            
+
+            // po svd
+            double[,] po_svd = new double[9, 9];
+            double x = 0;
+            for (int i = 0; i < 9 ; i++)
+                for (int j = i + 1; j < 9; j++)
+                {
+                    x = vt[i, j];
+                    vt[i, j] = vt[j, i];
+                    vt[j, i] = x;
+                }
+
+            double[,] nowe_u = new double[2, 12];
+            for (int i = 0; i < 2; i++)
+                for (int j = 0; j < 12; j++) nowe_u[i, j] = u[j, i];
+            double[,] nowe_vt = new double[9, 2];
+            for (int i = 0; i < 9; i++)
+                for (int j = 0; j < 2; j++) nowe_vt[i, j] = vt[j, i];
+            double[,] nowe_w = new double[2, 2];
+            nowe_w[0, 0] = w[0];
+            nowe_w[0, 1] = w[1];
+            nowe_w[1, 0] = w[2];
+            nowe_w[1, 1] = w[3];
+            double[,] u_w = new double[12, 2];
+            for (int i = 0; i < 12; i++)
+                for (int j = 0; j < 2; j++)
+                    for (int k = 0; k < 2; k++)
+                    u_w[i, j] += nowe_u[k, i] * nowe_w[k, j];
+
+            double[,] u_w_vt = new double[12, 9];
+            for (int i = 0; i < 12; i++)
+                for (int j = 0; j < 9; j++)
+                    for (int k = 0; k < 2; k++)
+                        u_w_vt[i, j] += u_w[i, k] * nowe_vt[j, k];
             for (int i = 0; i < 9; i++)
             {
-                dl_po_svd[i] = Math.Sqrt(Math.Pow(vt[i, 0], 2) + Math.Pow(vt[i, 1], 2));
+                for (int j = i + 1; j < 9; j++)
+                {
+                    for (int k = 0; k < 12; k++)
+                    {
+                        iloczyn = u_w_vt[k, i] * u_w_vt[k, j];
+                        iloczyn_wektorow[i, j] += iloczyn;
+                    }
+                    
+                }
+
             }
-            //double[,] odl_po_svd = new double[9, 9];
 
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 12; j++)
+                {
+                    dlugosci_wektorow[i] += Math.Pow(u_w_vt[j, i], 2);
+                }
+                dlugosci_wektorow[i] = Math.Sqrt(dlugosci_wektorow[i]);
+            }
 
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = i + 1; j < 9; j++)
+                {
+                    po_svd[i, j] = iloczyn_wektorow[i, j] / (dlugosci_wektorow[i] * dlugosci_wektorow[j]);
+                }
+            }
+            textBox2.Text += "\r\n";
+            textBox2.Text += "\r\n";
+            textBox2.Text += "\r\n";
+            textBox2.Text += "Po zastosowaniu SVD:\r\n";
+            textBox2.Text += "      C1  ||  C2  ||  C3  ||  C4  ||  C5  ||  M1  ||  M2  ||  M3\r\n";
+
+            textBox2.Text += do_wyswietlenia[0] + "  ";
+            textBox2.Text += Math.Round(po_svd[0, 1], 2);
+            textBox2.Text += "\r\n";
+
+            textBox2.Text += do_wyswietlenia[1] + "  ";
+            textBox2.Text += Math.Round(po_svd[0, 2], 2) + "  " + Math.Round(po_svd[1, 2], 2);
+            textBox2.Text += "\r\n";
+
+            textBox2.Text += do_wyswietlenia[2] + "  ";
+            textBox2.Text += Math.Round(po_svd[0, 3], 2) + "  " + Math.Round(po_svd[1, 3], 2) + "  " + Math.Round(po_svd[2, 3], 2);
+            textBox2.Text += "\r\n";
+
+            textBox2.Text += do_wyswietlenia[3] + "  ";
+            textBox2.Text += Math.Round(po_svd[0, 4], 2) + "  " + Math.Round(po_svd[1, 4], 2) + " " + Math.Round(po_svd[2, 4], 2) + " " + Math.Round(po_svd[3, 4], 2);
+            textBox2.Text += "\r\n";
+
+            textBox2.Text += do_wyswietlenia[4] + "  ";
+            textBox2.Text += Math.Round(po_svd[0, 5], 2) + " " + Math.Round(po_svd[1, 5], 2) + " " + Math.Round(po_svd[2, 5], 2) + " " + Math.Round(po_svd[3, 5], 2) + " " + Math.Round(po_svd[4, 5], 2);
+            textBox2.Text += "\r\n";
+
+            textBox2.Text += do_wyswietlenia[5] + "  ";
+            textBox2.Text += Math.Round(po_svd[0, 6], 2) + " " + Math.Round(po_svd[1, 6], 2) + " " + Math.Round(po_svd[2, 6], 2) + " " + Math.Round(po_svd[3, 6], 2) + " " + Math.Round(po_svd[4, 6], 2) + " " + Math.Round(po_svd[5, 6], 2);
+            textBox2.Text += "\r\n";
+
+            textBox2.Text += do_wyswietlenia[6] + "  ";
+            textBox2.Text += Math.Round(po_svd[0, 7], 2) + " " + Math.Round(po_svd[1, 7], 2) + " " + Math.Round(po_svd[2, 7], 2) + " " + Math.Round(po_svd[3, 7], 2) + " " + Math.Round(po_svd[4, 7], 2) + " " + Math.Round(po_svd[5, 7], 2) + " " + Math.Round(po_svd[6, 7], 2);
+            textBox2.Text += "\r\n";
+
+            textBox2.Text += do_wyswietlenia[7] + "  ";
+            textBox2.Text += Math.Round(po_svd[0, 8], 2) + " " + Math.Round(po_svd[1, 8], 2) + " " + Math.Round(po_svd[2, 8], 2) + " " + Math.Round(po_svd[3, 8], 2) + " " + Math.Round(po_svd[4, 8], 2) + " " + Math.Round(po_svd[5, 8], 2) + " " + Math.Round(po_svd[6, 8], 2) + " " + Math.Round(po_svd[7, 8], 2);
+            textBox2.Text += "\r\n";
         }
     }
 }
